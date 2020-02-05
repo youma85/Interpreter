39b4596ad04383e9f9aac2a9eb3057d00a90f17c
Rafii <rafiiyoussef@gmail.com>
2020-02-05 22:31:22 +0100

    First commit

---
+++ src/main/java/com/youraf/notebook/NotebookServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.youraf.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookServerAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/youraf/notebook/model/Command.java
@@ -0,0 +1,18 @@
+package com.youraf.notebook.model;
+
+public class Command {
+
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	
+	
+	
+}
